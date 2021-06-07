3328cb4fa65438c2da497390da199fcd8f0a394a
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-07 15:30:03 -0700

    brought in footer

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Footer from './components/footer/footer.js';
+
 const App = () => {
   return(
     <>
     <h1>Hey girl hey!</h1>
+    <Footer />
     </>
   );
 };
