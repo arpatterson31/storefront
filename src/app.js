cad2076b7c5e6170b3440f58be2980a4178abd94
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-07 16:46:21 -0700

    brought in categories component

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from './components/header/header.js';
 import Footer from './components/footer/footer.js';
+import Categories from './components/storefront/categories.js';
 
 const App = () => {
   return(
     <>
     <Header />
     <h1>Hey girl hey!</h1>
+    <Categories />
     <Footer />
     </>
   );
