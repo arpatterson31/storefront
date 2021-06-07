e4dc2e07d657dad59515cbad0d596d7aa1212507
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-07 15:30:21 -0700

    built out footer component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <section id="contact">
+          <h2>Contact Us</h2>
+          <ul>
+            <li>HQ: 95 Main Street, Seattle, WA 98101</li>
+            <li>Phone: 206-555-2374</li>
+            <li>Email: audrey@buymystuff.com</li>
+          </ul>
+        </section>
+        <p>&copy; 2021 Audrey-Code Fellows</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer;
