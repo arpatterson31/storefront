969944dc10a6e5e8d89204e7d82fff03b4f89cee
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-07 16:26:26 -0700

    brought in header component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Header from './components/header/header.js';
 import Footer from './components/footer/footer.js';
 
 const App = () => {
   return(
     <>
+    <Header />
     <h1>Hey girl hey!</h1>
     <Footer />
     </>
