675b8ddb7966c66ad885bf29c69b8f8a9700a36f
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-07 15:31:05 -0700

    setting up actions and reducers

---
+++ src/store/categories.js
@@ -0,0 +1,36 @@
+let initialState = {
+  category: [
+    { name: 'Electronics', displayName: 'ELECTRONICS', description: 'Electronic products' },
+    { name: 'Clothes', displayName: 'CLOTHES', description: 'Stuff you wear' },
+    { name: 'Food', displayName: 'FOOD', description: 'Things you can eat' },
+  ],
+  activeCategory: '',
+}
+
+export default function catReducer(state = initialState, action){
+  let { type, payload } = action;
+
+  switch(type){
+    case 'ACTIVATE':
+      return {...state, activeCategory: payload.category};
+
+    case 'INACTIVE':
+      return initialState;
+
+    default: 
+      return state;
+  }
+}
+
+export const activate = (category) => {
+  return {
+    type: 'ACTIVATE',
+    payload: category
+  }
+}
+
+export const inactivate = () => {
+  return {
+    type: 'INACTIVATE'
+  }
+}
