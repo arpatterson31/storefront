920691c41077cb5113c320a2b512534a67a8d278
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-07 22:03:52 -0700

    material ui added to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import { withStyles } from '@material-ui/core/styles';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 2px',
+  },
+}))(Badge);
 
 const Header = () => {
+  const classes = useStyles();
+
   return (
     <>
-      <header>
-        <h1>Audrey's Wares</h1>
+      <header className={classes.root}>
+        <AppBar position="static" color="secondary">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Audrey's Wares
+          </Typography>
+            <IconButton aria-label="cart">
+              <StyledBadge badgeContent={0} showZero color="primary">
+                <ShoppingCartIcon />
+              </StyledBadge>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
       </header>
     </>
   )
