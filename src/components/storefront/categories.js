0b718187f7bea946ebc1ca0b3437216eb2116c5b
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-07 22:39:52 -0700

    categories styled

--- src/components/storefront/categories.js
+++ src/components/storefront/categories.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { makeStyles } from '@material-ui/core/styles';
+
 import { connect } from 'react-redux';
 
 import { activate, inactivate } from '../../store/categories.js';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+}));
+
 const Category = props => {
+  const classes = useStyles();
+
   return (
     <section>
-      <h3>Browse our Categories</h3>
-      <ul>
+      <Typography variant="h5">Browse our Categories</Typography>
+      <div className={classes.root}>
         {props.categories.category.map(cat => (
-          <li onClick={() => props.activate(cat.name, cat.description)} key={cat.name}>{cat.displayName}</li>
+          <ButtonGroup variant="text" size="small" color="primary" aria-label="text primary button group" key={cat.name}>
+            <Button onClick={() => props.activate(cat.name, cat.description)}>{cat.displayName}</Button>
+          </ButtonGroup> 
         ))}
-      </ul>
+      </div>
     </section>
   )
 }
