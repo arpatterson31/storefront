3694cf89304422b79006f4242b75b476f772c68a
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-08 16:34:42 -0700

    brought in simplecart component

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ import Header from './components/header/header.js';
 import Footer from './components/footer/footer.js';
 import Categories from './components/storefront/categories.js';
 import Products from './components/storefront/products.js';
+import SimpleCart from './components/cart/simplecart.js';
 
 const App = () => {
   return(
     <>
     <Header />
+    <SimpleCart />
     <Categories />
     <Products />
     <Footer />
