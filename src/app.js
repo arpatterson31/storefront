badca502e6a392078c764fb7c8b495ba56840980
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-07 20:30:35 -0700

    bringing in products components

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/header/header.js';
 import Footer from './components/footer/footer.js';
 import Categories from './components/storefront/categories.js';
+import Products from './components/storefront/products.js';
 
 const App = () => {
   return(
@@ -10,6 +11,7 @@ const App = () => {
     <Header />
     <h1>Hey girl hey!</h1>
     <Categories />
+    <Products />
     <Footer />
     </>
   );
