05c51769c77d90dbf7eee927e5c4cc93f6c6e927
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-11 21:17:16 -0700

    items are removing from the cart..hallelujah

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,3 +1,7 @@
+import superagent from 'superagent';
+
+let api = 'https://audrey-api-server.herokuapp.com/products';
+
 let initialState = {
   cart: []
 }
@@ -5,16 +9,16 @@ let initialState = {
 export default function cartReducer(state = initialState, action) {
   let { type, payload } = action;
 
-  switch(type) {
+  switch (type) {
     case 'ADD_TO_CART':
-      return {...state, cart: [...state.cart, payload]}
+      return { ...state, cart: [...state.cart, payload] }
 
     case 'REMOVE_FROM_CART':
-      return [...state.filter((item, idx) => {
-        if (idx !== payload.idx) {
-            return item;
-        }
-    })]
+      let cart = [...state.cart]
+      let removedItem = payload.idx;
+      cart.splice(removedItem, 1);
+
+      return { cart };
 
     default:
       return state;
@@ -31,7 +35,7 @@ export const decrementRemoteData = (data) => dispatch => {
 
 export const incrementRemoteData = (data, idx) => dispatch => {
   data.count++;
-  return (await superagent.put(`${api}/${data._id}`)).send(data)
+  return superagent.put(`${api}/${data._id}`).send(data)
     .then(response => {
       dispatch(removeFromCart(response.body, idx));
     })
@@ -44,7 +48,7 @@ export const addToCart = (product) => {
   }
 }
 
-export const removeFromCart = (product) => {
+export const removeFromCart = (product, idx) => {
   return {
     type: 'REMOVE_FROM_CART',
     payload: { product, idx }
