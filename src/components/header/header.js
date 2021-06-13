2b1d2042d6891717b1a3ea67f2c759b029893242
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-12 21:55:26 -0700

    link to home route when onclick of header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
 import { withStyles } from '@material-ui/core/styles';
@@ -17,6 +18,8 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
+    textDecoration: "none",
+    color: "white"
   },
 }));
 
@@ -38,7 +41,9 @@ const Header = (props) => {
       <header className={classes.root}>
         <AppBar position="static" color="secondary">
           <Toolbar>
-            <Typography variant="h6" className={classes.title}>
+            <Typography 
+            component={Link} to="/"
+            variant="h6" className={classes.title}>
               Audrey's Wares
           </Typography>
             <IconButton aria-label="cart">
