e91aa30cd5dd629e9eda4bebb7bd0d22985565b6
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-12 22:48:35 -0700

    starting of checkout..cart page

--- src/components/cart/checkout.js
+++ src/components/cart/checkout.js
@@ -1,10 +1,63 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { incrementRemoteData } from '../../store/cart';
+
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    margin: "auto",
+    width: "50%"
+  },
+  title: {
+    flex: '1 1 100%',
+    margin: "20px 0 10px 5px",
+    padding: "10px"
+  },
+  items: {
+    margin: "0 0 5px 5px"
+  }
+
+}));
 
 const ShoppingCart = props => {
+  const classes = useStyles();
+
+
   return (
     <>
+
+      <Container className={classes.container}>
+        <Paper>
+
+          <Typography className={classes.title} variant="h6" component="div">
+            Order Summary
+            </Typography>
+          {props.cart.cart.map((item, idx) => (
+            <Grid key={idx} container spacing={1} className={classes.items}>
+              <Grid item xs={8}>
+                <Typography  variant="body2">{item.name}</Typography>
+              </Grid>
+
+              <Grid item xs={3}>
+                <Typography  variant="body2" align="right">${item.price}</Typography>
+              </Grid>
+            </Grid>
+          ))}
+          <Grid item xs={3}>
+          <Typography>Total: </Typography>
+          <Typography></Typography>
+          </Grid>
+
+        </Paper>
+
+      </Container>
     </>
   )
 }
@@ -13,6 +66,5 @@ const mapStateToProps = state => ({
   cart: state.cart,
 })
 
-const mapDispatchToProps = { incrementRemoteData }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCart);
+export default connect(mapStateToProps)(ShoppingCart);
