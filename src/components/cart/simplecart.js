93024e8bc560756879d8aced73b56e44bae99f2a
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-12 21:55:59 -0700

    added onclick to cart item to go to product details

--- src/components/cart/simplecart.js
+++ src/components/cart/simplecart.js
@@ -5,18 +5,19 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import HighlightOffRoundedIcon from '@material-ui/icons/HighlightOffRounded';
-import IconButton from '@material-ui/core/IconButton';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import Grid from '@material-ui/core/Grid';
 
 import { connect } from 'react-redux';
 import { incrementRemoteData } from '../../store/cart.js';
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 160,
-    fontSize: 12,
+    maxWidth: 500,
+    fontSize: 10,
     position: 'absolute',
-    right: 8
+    right: 8,
+
   }
 });
 
@@ -26,22 +27,22 @@ const SimpleCart = props => {
   if (props.cart.cart.length > 0) {
     return (
       <>
-        <div>
-          <Card className={classes.root} raised>
-            <CardContent>
-              {props.cart.cart.map((item, idx) => (
-                <Typography key={idx} variant="body2" component={Link} to={`/product/${item._id}`}>
-                  {item.name}
-                  <IconButton color="secondary" edge="end" aria-label="delete" onClick={() => props.incrementRemoteData(item, idx)}>
-                    <HighlightOffRoundedIcon />
-                  </IconButton>
-                </Typography>
-              ))}
-              </CardContent>
-
-          </Card>
-
-        </div>
+        <Card className={classes.root} raised>
+          <CardContent>
+            {props.cart.cart.map((item, idx) => (
+              <Grid container spacing={3}>
+                <Grid item md={8}>
+                  <Typography key={idx} variant="body2" display="inline" component={Link} to={`/product/${item._id}`} style={{ textDecoration: "none", color: "black" }}>
+                    {item.name}
+                  </Typography>
+                </Grid>
+                <Grid item md={2}>
+                  <DeleteOutlinedIcon color="secondary" edge="end" fontSize="small" aria-label="delete" onClick={() => props.incrementRemoteData(item, idx)} />
+                </Grid>
+              </Grid>
+            ))}
+          </CardContent>
+        </Card>
       </>
     )
   } else {
